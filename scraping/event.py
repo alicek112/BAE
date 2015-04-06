80831ad1ea8e3cf6558284eb9c6c357c55b3fe6e
Alice <alicek112@gmail.com>
2015-04-06 15:06:09 -0400

    event module

---
+++ scraping/event.py
@@ -0,0 +1,23 @@
+'''
+Event object.
+
+Created by Alice Kroutikova '15
+'''
+import datetime
+
+class Event(object):
+    name = ""
+    starttime = datetime.datetime.now()
+    endtime = datetime.datetime.now()
+    location = ""
+    
+    def __init__(self, name, starttime, endtime, location):
+        self.name = name 
+        self.starttime = starttime
+        self.endttime = endtime
+        self.location = location 
+    
+    def __str__(self):
+        return self.name + '\n' + str(self.starttime) + '---' + str(self.endtime) + '\n' + self.location
+    
+    
