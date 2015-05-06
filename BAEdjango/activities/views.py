b48d190b1a8b41ee7887283c3ee095321a7a8a90
Serena Zheng <serenaz@princeton.edu>
2015-05-06 10:20:27 -0400

    CAS authentication

--- BAEdjango/activities/views.py
+++ BAEdjango/activities/views.py
@@ -7,25 +7,29 @@ from .models import Mainbae
 from django.contrib.auth import authenticate, login
 
 def index(request):
-	#if not request.user.is_authenticated():
-	#	user = authenticate()
-	#	if user is not None:
-	#		login(request, user)
-	#		context = RequestContext(request)
-	#		template = loader.get_template('activities/index.html')
-	#		return HttpResponse(template.render(context))
-	#	else:
-	#		return HttpResponse("invalid login")
-	#else:
-		allcats = Catlist.objects.all().order_by('name')
-		context = RequestContext(request, {'allcats': allcats})
-		template = loader.get_template('activities/index.html')
-		return HttpResponse(template.render(context))
+	if not request.user.is_authenticated():
+		user = authenticate()
+		if user is not None:
+			login(request, user)
+		else:
+			return HttpResponse("invalid login")
+
+	allcats = Catlist.objects.all().order_by('name')
+	context = RequestContext(request, {'allcats': allcats})
+	template = loader.get_template('activities/index.html')
+	return HttpResponse(template.render(context))
 
 
 	
 
 def cat(request, cat_id):
+	if not request.user.is_authenticated():
+		user = authenticate()
+		if user is not None:
+			login(request, user)
+		else:
+			return HttpResponse("invalid login")
+			
     try:
        catl = Catlist.objects.get(pk = cat_id)
     except Catlist.DoesNotExist:
